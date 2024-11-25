783db601a3e57a9eb1b59353841aef70325b2911
AnatarX <flameelite88@mail.ru>
2024-11-25 18:20:18 +0300

    add markdown to html lib

--- scripts/opportunity.js
+++ scripts/opportunity.js
@@ -5,8 +5,7 @@ function getDescription() {
     })
     .then(async (response) => {
         if (response.status === 200) {
-            descriptionContainer.innerHTML = await response.text()
-            // ...
+            descriptionContainer.innerHTML = (new showdown.Converter()).makeHtml(await response.text())
             return
         }
         descriptionContainer.textContent = "Some error occured, refresh page to see opportunity description"
